970833af36796d70521c2cc0d0fbf3eba49f2d53
AleefCma <alefedonato777hotmail.com@gmail.com>
2023-12-06 18:46:19 -0300

    finalização do projeto implemntação das funções dos botões nex prev

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,14 @@ const imagePokemon = document.querySelector('.pokemon_image')
 const form = document.querySelector('.form')
 const formInput = document.querySelector('.input_search')
 
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1
+
 const fetchPokemon = async (pokemon) => {
 
-    const ApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+    const ApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
     const data = await ApiResponse.json()
     return data
@@ -17,25 +22,40 @@ const fetchPokemon = async (pokemon) => {
 const renderPokemon = async (pokemon) => {
     try {
         const data = await fetchPokemon(pokemon)
-
+        imagePokemon.style.display = 'block'
         namePokemon.innerHTML = data.name
         idPokemon.innerHTML = data.id
         imagePokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
         formInput.value = ''
+        searchPokemon = data.id
     }
     catch (error) {
 
-        formInput.value = 'pokemon não existe'
+        imagePokemon.style.display = 'none'
+        namePokemon.innerHTML = 'pokemon não existe'
 
     }
 }
-    form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => {
 
-        event.preventDefault()
+    event.preventDefault()
 
-        renderPokemon(formInput.value)
+    renderPokemon(formInput.value.toLowerCase())
 
 
-    })
+})
 
-    renderPokemon('1')
+buttonNext.addEventListener('click', () => {
+    searchPokemon = searchPokemon + 1
+    renderPokemon(searchPokemon)
+
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+
+        searchPokemon = searchPokemon - 1
+        renderPokemon(searchPokemon)
+    }
+})
+renderPokemon(searchPokemon)
